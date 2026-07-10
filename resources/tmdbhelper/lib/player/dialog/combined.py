dbde1fc0b51e4fb0f7c32bd26b0fd0fe434887e3
martin1080p <fantamartin@centrum.cz>
2026-07-10 22:25:41 +0200

    chore(addon): bump version to 6.16.5 fix(player): simplify player selection logic in combined player dialog

--- resources/tmdbhelper/lib/player/dialog/combined.py
+++ resources/tmdbhelper/lib/player/dialog/combined.py
@@ -12,8 +12,6 @@ class PlayerSelectCombined(PlayerSelectStandard):
     def select_from_group(self, group, header=None, detailed=True):
         if len(self.players_generated_list) != 1:
             return super().select(header, detailed)
-        if group.player.plugin_name != 'plugin.video.themoviedb.helper':
-            return super().select(header, detailed)
         return self.get_player(group.posx)
 
     def set_current_group(self, x):
